c4cc6bb0fd36b3e52934678de58abd93eb56ec5d
whatthefoobar <irina.mntn@gmail.com>
2023-01-24 17:20:32 +0100

    cleanup

--- src/pages/MoviePage.js
+++ src/pages/MoviePage.js
@@ -6,22 +6,18 @@ import axios from "axios";
 const MoviePage = () => {
   const params = useParams();
   const { id } = params;
-  console.log(id);
 
-  //   const search_Api = `https://api.themoviedb.org/3/search/movie?&api_key=81d9405cdcc5bb67fea79273223ddabd&query=${id}`;
   const search_Api = `https://api.themoviedb.org/3/movie/${id}?api_key=81d9405cdcc5bb67fea79273223ddabd&language=en-US`;
 
   const [movie, setMovie] = useState({});
   const fetchData = async (API) => {
     const result = await axios(API);
 
-    setMovie(result.data); // filters out null properties
-    // console.log(movie.title);
+    setMovie(result.data);
   };
 
   useEffect(() => {
     fetchData(search_Api);
-    // console.log(movie);
   }, [id]);
 
   return <div>{movie.title}</div>;
