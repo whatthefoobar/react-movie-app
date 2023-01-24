ed8d9400ce0c571b2e5bb9291bf4fbc3c8f77496
whatthefoobar <irina.mntn@gmail.com>
2023-01-24 17:19:32 +0100

    fetched indiv movie now let's just style a page

--- src/pages/MoviePage.js
+++ src/pages/MoviePage.js
@@ -1,12 +1,30 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const MoviePage = () => {
   const params = useParams();
   const { id } = params;
   console.log(id);
-  // fetch movie by id from params
-  return <div>MoviePage</div>;
+
+  //   const search_Api = `https://api.themoviedb.org/3/search/movie?&api_key=81d9405cdcc5bb67fea79273223ddabd&query=${id}`;
+  const search_Api = `https://api.themoviedb.org/3/movie/${id}?api_key=81d9405cdcc5bb67fea79273223ddabd&language=en-US`;
+
+  const [movie, setMovie] = useState({});
+  const fetchData = async (API) => {
+    const result = await axios(API);
+
+    setMovie(result.data); // filters out null properties
+    // console.log(movie.title);
+  };
+
+  useEffect(() => {
+    fetchData(search_Api);
+    // console.log(movie);
+  }, [id]);
+
+  return <div>{movie.title}</div>;
 };
 
 export default MoviePage;
